dadaeab4c1dbae14f199f0df1510dc89bfbc48db
davojs71 <djsp@ymail.com>
2026-08-04 20:46:00 +0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,8 +1,25 @@
+import os
+import requests
 from datetime import datetime
 
+
 def main():
-    print("AFL Player Analytics Agent running")
-    print("Time:", datetime.now())
+
+    url = os.environ["GOOGLE_SCRIPT_URL"]
+
+    payload = {
+        "season": "2026",
+        "player": "SYSTEM TEST",
+        "test": "Connection successful",
+        "time": str(datetime.now())
+    }
+
+    response = requests.post(
+        url,
+        json=payload
+    )
+
+    print(response.text)
 
 
 if __name__ == "__main__":
